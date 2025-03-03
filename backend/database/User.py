94d3b931da867c02c9b98adf0ceb57113fc584ef
gbbelskij <bleb_1@mail.ru>
2025-03-03 16:07:38 +0300

    Autentification

--- backend/database/User.py
+++ backend/database/User.py
@@ -1,6 +1,8 @@
 from sqlalchemy.dialects.postgresql import JSONB
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
 db = SQLAlchemy()
@@ -8,7 +10,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
     
-    user_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
