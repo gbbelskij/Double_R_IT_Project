6b231ea26a51e0f82efc0f2b5a9a335e92440477
gleb_belskij <102985700+gbbelskij@users.noreply.github.com>
2025-03-07 09:37:23 +0300

    Autentification (#8)
    * Autentification
    
    * Удалил тестовую ручку

--- app.py
+++ app.py
@@ -4,19 +4,37 @@ import config
 from backend.database.User import db
 from flask_restx import Api
 from backend.handlers.register import register_ns
+from backend.handlers.login import login_ns
 
 app = Flask(__name__)
 
 # Пример URL для подключения
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['JWT_ALGORITHM'] = config.JWT_ALGORITHM
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
-api = Api(app, version="1.0", title="RRecomend API",
-          description="API сервиса RRecomend")
+api = Api(
+    app,
+    version="1.0",
+    title="RRecomend API",
+    description="API сервиса RRecomend",
+    security=[{'BearerAuth': []}],
+    authorizations={
+        'BearerAuth': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization'
+        }
+    }
+)
+
+
 api.add_namespace(register_ns, path='/register')
+api.add_namespace(login_ns, path='/login')
 
 def create_tables():
     db.create_all()
--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 DATABASE_URL="postgresql+psycopg2://myuser:mypassword@172.19.0.2:5432/RRecomend_db"
+SECRET_KEY = 'MAI_IS_THE_BEST_PLACE_FOR_VIPERRS'
+JWT_ALGORITHM = 'HS256'
