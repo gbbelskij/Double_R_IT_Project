f6ab1b6051908b82ac0a0b75488761453fd312c0
gbbelskij <bleb_1@mail.ru>
2025-03-09 13:21:38 +0300

    environ

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-DATABASE_URL="postgresql+psycopg2://myuser:mypassword@172.19.0.2:5432/RRecomend_db"
-SECRET_KEY = 'MAI_IS_THE_BEST_PLACE_FOR_VIPERRS'
-JWT_ALGORITHM = 'HS256'
+import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql+psycopg2://myuser:mypassword@localhost:5432/RRecomend_db")
+SECRET_KEY = os.environ.get("SECRET_KEY", 'MAI_IS_THE_BEST_PLACE_FOR_VIPERRS')
+JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM", 'HS256')
