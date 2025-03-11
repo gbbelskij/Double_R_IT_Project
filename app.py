1932a7e4aeb24bf9e396bfdfd0ccce2974c251f4
gleb_belskij <102985700+gbbelskij@users.noreply.github.com>
2025-03-11 14:09:31 +0300

    add logout (#17)

--- app.py
+++ app.py
@@ -6,37 +6,45 @@ from flask_restx import Api
 from backend.handlers.register import register_ns
 from backend.handlers.login import login_ns
 from backend.handlers.personal_account import personal_account_ns
+from flask_jwt_extended import JWTManager
 
-app = Flask(__name__)
 
-# Пример URL для подключения
-app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = config.SECRET_KEY
-app.config['JWT_ALGORITHM'] = config.JWT_ALGORITHM
+def create_app():
+    app = Flask(__name__)
 
-db.init_app(app)
-migrate = Migrate(app, db)
+    # Пример URL для подключения
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = config.SECRET_KEY
+    app.config['JWT_ALGORITHM'] = config.JWT_ALGORITHM
 
-api = Api(
-    app,
-    version="1.0",
-    title="RRecomend API",
-    description="API сервиса RRecomend",
-    security=[{'BearerAuth': []}],
-    authorizations={
-        'BearerAuth': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization'
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    api = Api(
+        app,
+        version="1.0",
+        title="RRecomend API",
+        description="API сервиса RRecomend",
+        security=[{'BearerAuth': []}],
+        authorizations={
+            'BearerAuth': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+            }
         }
-    }
-)
+    )
 
+    jwt = JWTManager(app)
 
-api.add_namespace(register_ns, path='/register')
-api.add_namespace(login_ns, path='/login')
-api.add_namespace(personal_account_ns, path='/personal_account')
+    api.add_namespace(register_ns, path='/register')
+    api.add_namespace(login_ns, path='/login')
+    api.add_namespace(personal_account_ns, path='/personal_account')
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=5000)
--- backend/app/jwt.py
+++ backend/app/jwt.py
@@ -1,24 +1,21 @@
-import jwt
-from datetime import datetime, timedelta
-
+from datetime import timedelta
+from flask_jwt_extended import (
+    create_access_token, decode_token
+)
 
 def create_jwt_token(user_id):
-    """Создаем JWT токен для пользователя"""
-    from app import app
-    payload = {
-        'user_id': str(user_id),
-        'exp': datetime.utcnow() + timedelta(hours=24)  # Токен действует 24 часа
-    }
-    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm=app.config['JWT_ALGORITHM'])
-    return token
+    """Создаем JWT токен для пользователя с помощью Flask-JWT-Extended"""
+    # Создаем токен доступа (access token), срок действия 24 часа
+    access_token = create_access_token(identity=user_id, expires_delta=timedelta(hours=24))
+    return access_token
+
+
 
 def verify_jwt_token(token):
-    """Проверяем JWT токен"""
-    from app import app
+    """Проверяем JWT токен с помощью Flask-JWT-Extended"""
     try:
-        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=[app.config['JWT_ALGORITHM']])
-        return payload['user_id']
-    except jwt.ExpiredSignatureError:
-        return None  # Токен истек
-    except jwt.InvalidTokenError:
-        return None  # Неверный токен
+        decoded_token = decode_token(token)
+        return decoded_token['sub']  # 'sub' содержит user_id
+    except Exception as e:
+        print(f"Error verifying token: {str(e)}")
+        return None
