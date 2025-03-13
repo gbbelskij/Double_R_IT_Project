70d2436526c5c7d46c6200b5463db0c7e08836dd
e1n4ach <ilya-sitnikov-2020@list.ru>
2025-03-13 13:55:18 +0300

    component input

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Logo from "../components/Logo/Logo";
 import Main from "../components/Main/Main";
-import Input from "../components/Input/Input";
 
 import "./HomePage.css";
 
@@ -12,14 +11,6 @@ export default function Home() {
       <Header />
       <Main>
         <Logo hasText />
-        <div>
-          <Input 
-            type="number" 
-            name="experience" 
-            label="Опыт работы" 
-            unit="лет"
-          />
-        </div>
       </Main>
     
       <Footer />
